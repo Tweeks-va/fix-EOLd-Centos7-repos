a1d79ecd9508704264fb0efa0c16f8259f2c1526
Thomas Weeks <tweeks-homework@theweeks.org>
2024-10-31 10:19:14 -0400

    Update centos-7-yum-repo-fix.sh
    made more curl/lynx friendly

--- centos-7-yum-repo-fix.sh
+++ centos-7-yum-repo-fix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## By tweeks@VirginiaCyberRange.org
-## WARNING: This script disables all CentOS7 yum repos except the Vault C7.8.2003 repos.
-## DO NOT RUN THIS on any distro other than CentOS7.
+## WARNING: This script disables all CentOS7 yum repos except the 
+## Vault C7.8.2003 repos.DO NOT RUN THIS on any distro other than CentOS7.
 ## Must run as root.
 
 if [[ $UID != "0" ]]; then
@@ -10,13 +10,14 @@ if [[ $UID != "0" ]]; then
 fi
 echo
 echo
-## Show yum list status.. Comment out the "read" line if you want to run this script
-## with no human interaction.
+## Show yum list status.. 
 yum list ; echo "STATUS=$?"
 echo "If STATUS=1, then yum is not working.  If STATUS=0, then I recommend"
 echo "hitting CTRL-C and aborting this..."
 echo "... hit enter if you're sure you want to contine..."
 ## Pauses for human input (ENTER to run.. CTRL-C to abort)
+## Comment out the "read" line if you want to run this script
+## with no human interaction.
 read x
 if [ -f /etc/yum.repos.d/CentOS-Vault.repo ]; then
 	cd /etc/yum.repos.d/
